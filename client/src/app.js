3a56a3e439314c04d5e7910d45a117d0e31b5211
Bearcin46 <imsweety33@gmail.com>
2024-03-21 08:16:51 +0530

    feat : client side routing

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { About } from "./Components/About";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Contact from "./Components/Contact";
 import Error from "./Components/Error";
 
@@ -13,20 +13,31 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <AppLayout />, errorElement: <Error /> },
   {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+
+    errorElement: <Error />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("app"));
